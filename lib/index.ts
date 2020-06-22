710f6cc95080248851c3678275370a8a2e0aa506
Madis Kariler <madis.kariler@gmail.com>
2020-06-23 02:23:44 +0300

    Change default length to 24 char

--- lib/index.ts
+++ lib/index.ts
@@ -1,7 +1,7 @@
 import { GenericServiceCall, NextFunction, GenericCallHandler } from '@mdkr/grpc-chain';
 import * as crypto from 'crypto';
 
-export default function (length = 12): GenericCallHandler {
+export default function (length = 24): GenericCallHandler {
   return (call: GenericServiceCall, next: NextFunction): void => {
     call.ctx.locals.reqId = crypto
       .randomBytes(Math.ceil((length * 3) / 4))
