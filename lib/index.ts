9ce0fd14e14551684730b729dfa9abcb20152a8e
Madis Kariler <madis.kariler@gmail.com>
2020-06-24 22:26:34 +0300

    Make compatible with @mdkr/grpc-chain v0.3.0

--- lib/index.ts
+++ lib/index.ts
@@ -1,14 +1,14 @@
-import { GenericServiceCall, NextFunction, GenericCallHandler } from '@mdkr/grpc-chain';
+import { GenericServiceCall, GenericCallHandler, ReadyFunction } from '@mdkr/grpc-chain';
 import * as crypto from 'crypto';
 
 export default function (length = 24): GenericCallHandler {
-  return (call: GenericServiceCall, next: NextFunction): void => {
+  return (call: GenericServiceCall, ready: ReadyFunction): void => {
     call.ctx.locals.reqId = crypto
       .randomBytes(Math.ceil((length * 3) / 4))
       .toString('base64')
       .slice(0, length)
       .replace(/\+/g, '0')
       .replace(/\//g, '0');
-    next();
+    ready();
   };
 }
